6a750cea23dd717d057604eb8c1e241e3b877fab
André <andrevictor50@gmail.com>
2021-01-12 10:25:28 -0300

    reading all content from all srt files

--- src/functions/index.js
+++ src/functions/index.js
@@ -21,7 +21,25 @@ function elementsEndingWith(array, pattern) {
   return array.filter((element) => element.endsWith(pattern))
 }
 
+function readFile(path) {
+  return new Promise((resolve, reject) => {
+    try {
+      const content = fs.readFileSync(path, { encoding: 'utf-8' });
+
+      resolve(content.toString());
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+function readFiles(paths) {
+  return Promise.all(paths.map((path) => readFile(path)))
+}
+
 module.exports = {
   readDirectoryFiles,
   elementsEndingWith,
+  readFile,
+  readFiles,
 }
